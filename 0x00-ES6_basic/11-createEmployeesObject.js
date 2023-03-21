650b7d60847171135293945c1028ff542bcea6ad
SemTheLegend <semmathe@gmail.com>
2023-03-21 09:23:06 -0700

    task 11 - Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,11 @@
 export default function createEmployeesObject(departmentName, employees) {
+  let employeeList = [];
+  for (let employee of employees) {
+    employeeList.append(employee);
+  }
+  const obj = {
+    departmentName: employeeList
+  };
 
+  return obj;
 }
