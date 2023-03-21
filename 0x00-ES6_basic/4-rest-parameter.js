097aea8e48cb7d11d7a8f237248c1696fb004009
SemTheLegend <semmathe@gmail.com>
2023-03-21 07:13:46 -0700

    made changes to the for loop on task 4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...args) {
   let count = 0;
-  for (let x = 0; x <= args.length; x++) {
+  for (let x = 0; x < args.length; x++) {
     count++;
   }
   return count;
