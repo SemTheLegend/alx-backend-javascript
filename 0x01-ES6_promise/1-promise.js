8a399ede4384e9c092765ae2eb66966070db8ce6
SemTheLegend <semmathe@gmail.com>
2023-03-22 07:27:28 -0700

    removed i next to arrow

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise(function(resolve, reject) => i{
+  return new Promise(function(resolve, reject) => {
     if (success === true) {
-      return resolve({
-	status: 200, body: 'Success' 
-      });
-    } else if (success === false) {
-      return reject({
-        "The fake API is not working currently"
-      });
+      return resolve({ status: 200, body: 'Success' });
+    } else {
+      return reject({"The fake API is not working currently"});
     }
   });
 }
