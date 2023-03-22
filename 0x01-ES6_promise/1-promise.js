cb04ee7e6e8c56e6a68a180bd4899f07366fb979
SemTheLegend <semmathe@gmail.com>
2023-03-22 07:34:46 -0700

    added return statement

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise(function(resolve, reject){
+  return new Promise(function(resolve, reject) {
     if (success === true) {
-      resolve({ status: 200, body: 'Success' });
+     return resolve({ status: 200, body: 'Success' });
     } else {
       reject({"The fake API is not working currently"});
     }
