76e492c479ac50c8b8a1b7166e1545383d902862
SemTheLegend <semmathe@gmail.com>
2023-03-22 07:40:10 -0700

    fixed a parsing error

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise(function(resolve, reject) {
     if (success === true) {
-     return resolve({ status: 200, body: 'Success' });
+      return resolve({ status: 200, body: 'Success' });
     } else {
-      reject({"The fake API is not working currently"});
+      reject("The fake API is not working currently");
     }
   });
 }
