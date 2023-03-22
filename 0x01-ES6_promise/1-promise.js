a20e5104fcd2031df80291576cca880cebbed85b
SemTheLegend <semmathe@gmail.com>
2023-03-22 07:41:33 -0700

    added return statement to reject

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success === true) {
       return resolve({ status: 200, body: 'Success' });
     } else {
-      reject("The fake API is not working currently");
+      return reject("The fake API is not working currently");
     }
   });
 }
