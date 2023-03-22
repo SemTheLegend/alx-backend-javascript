2353287538079c46db4c53a5306273f32dee405e
SemTheLegend <semmathe@gmail.com>
2023-03-22 07:23:37 -0700

    made changes to file 1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,13 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) => i{
     if (success === true) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject("The fake API is not working currently");
+      resolve({
+	status: 200, body: 'Success' 
+      });
+    } else if (success === false) {
+      reject({
+        "The fake API is not working currently"
+      });
     }
   });
 }
