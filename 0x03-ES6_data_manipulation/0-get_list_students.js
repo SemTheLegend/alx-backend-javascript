3d4c1ae59e56dc4b420a40277eb927705fd6083e
SemTheLegend <semmathe@gmail.com>
2023-03-28 21:55:55 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-  const arrayList = [
-    { id: 1, firstName: 'Guilluame', location: 'San Franciso' },
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return arrayList;
 }
