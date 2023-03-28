d4415a970ab3da07b2682b1bcfee965c53be1e7a
SemTheLegend <semmathe@gmail.com>
2023-03-28 21:57:32 +0000

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,6 @@
 export default function getListStudentIds(array) {
-  const result = Array.isArray(array);
-
-  if (result) {
-    const newList = [];
-    for (let x = 0; x < array.length; x++) {
-      newList.push(array[x].id);
-    }
-    return newList;
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
   }
-  const emptyArray = [];
-  return emptyArray;
+  return [];
 }
