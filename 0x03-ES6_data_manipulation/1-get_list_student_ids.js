6e80e6c0af90d48345b4b9e45aa74394cce2ec35
SemTheLegend <semmathe@gmail.com>
2023-03-28 21:12:33 +0000

    made some updates to the if statement

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,11 @@ export default function getListStudentIds(array) {
   const result = Array.isArray(array);
 
   if (result) {
-    return (`${array} is an array.`);
+    const newList = [];
+    for (let x = 0; x < array.length; x++) {
+      newList.push(array[x].id);
+    }
+    return newList;
   }
   const emptyArray = [];
   return emptyArray;
